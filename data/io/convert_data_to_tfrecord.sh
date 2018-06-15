221dd36218c38539e2ab28ab3ad8752c013e46b2
lizhuow <lizhuow@126.com>
2018-06-15 20:13:50 +0800

    first_commit

---
+++ data/io/convert_data_to_tfrecord.sh
@@ -0,0 +1,8 @@
+python convert_data_to_tfrecord.py \
+  --VOC_dir=/home/qinxx/lzw/thesis/DOTA/train_split/ \
+  --xml_dir=labelTxt \
+  --image_dir=images \
+  --save_dir=/home/qinxx/lzw//thesis/DOTA/train_split/ \
+  --dataset=train_split \
+  --save_name=ship_server \
+  --img_format=.png \
---
+++ data/io/convert_data_to_tfrecord_txt_debug.sh
@@ -0,0 +1,10 @@
+python convert_data_to_tfrecord_txt_debug.py \
+  --VOC_dir=/home/qinxx/lzw/thesis/DOTA/train_split/further_50/ \
+  --xml_dir=labelTxt \
+  --image_dir=images \
+  --save_dir=/home/qinxx/lzw//thesis/DOTA/train_split/further_50/ \
+  --dataset=further_50_900_1100 \
+  --save_name=ship_server \
+  --txt_file=/home/qinxx/lzw/thesis/DOTA/train_split/further_50/no_need_to_split_list.txt \
+  --txt_VOC_dir=/home/qinxx/lzw/thesis/DOTA/train_split/ \
+  --img_format=.png \
